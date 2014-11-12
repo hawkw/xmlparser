0e5a63e91be8b196c53f76262b536739346ea1ff
Hawk Weisman <hawk@meteorcodelabs.com>
2014-11-12 16:19:26 -0500

    Add empty source files

---
+++ src/main/scala/xmlparser/XMLParser.scala
@@ -0,0 +1,10 @@
+package xmlparser
+
+import scala.util.parsing.combinator.{PackratParsers, Parsers}
+
+/**
+ * Created by hawk on 11/12/14.
+ */
+object XMLParser extends Parsers with PackratParsers {
+
+}
---
+++ src/main/scala/xmlparser/package.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by hawk on 11/12/14.
+ */
+package object xmlparser {
+
+}
